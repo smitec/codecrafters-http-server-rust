32e9fd33c31f91e3e1535d8112f157815736c55c
Elliot Smith <elliot@elliotcsmith.com>
2024-04-06 08:25:46 +1000

    Path matching

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 // Uncomment this block to pass the first stage
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
+
+enum HttpMethod {
+    GET,
+}
+
+fn parse_method(content: &str) -> Option<HttpMethod> {
+    match content {
+        "GET" => Some(HttpMethod::GET),
+        _ => None,
+    }
+}
+
+struct StartLine {
+    method: HttpMethod,
+    path: String,
+    version: String,
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,9 +32,39 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                stream
-                    .write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
-                    .expect("Couldn't write bytes!");
+                // Read from the stream.
+
+                let mut buffer = [0_u8; 4096];
+
+                // TODO: Partial reads?
+                let read = stream
+                    .read(&mut buffer)
+                    .expect("Couldn't read from stream!");
+
+                let content = std::str::from_utf8(&buffer).unwrap().to_string();
+
+                let (start_line, _other) = content.split_once("\r\n").unwrap();
+                let mut parts = start_line.splitn(3, ' ');
+
+                // TODO: less lazy error handling
+                let start_line = StartLine {
+                    method: parse_method(parts.next().unwrap()).unwrap(),
+                    path: parts.next().unwrap().to_string(),
+                    version: parts.next().unwrap().to_string(),
+                };
+
+                match start_line.path.as_str() {
+                    "/" => {
+                        stream
+                            .write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+                            .expect("Couldn't write bytes!");
+                    }
+                    _ => {
+                        stream
+                            .write_all("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes())
+                            .expect("Couldn't write bytes!");
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
