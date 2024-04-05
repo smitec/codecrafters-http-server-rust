755338a87520ce972775ff386136f0f0559a1335
Elliot Smith <elliot@elliotcsmith.com>
2024-04-06 07:56:45 +1000

    Write back the header

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,8 +11,10 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                stream
+                    .write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+                    .expect("Couldn't write bytes!");
             }
             Err(e) => {
                 println!("error: {}", e);
